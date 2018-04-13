88a4e94f8419c27923d4d9dd804820aff9fff5d2
dimitrins <38352766+dimitrins@users.noreply.github.com>
2018-04-13 14:42:47 +0200

    Create restServer.py

---
+++ restServer.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from flask.ext.jsonpify import jsonify
+from rest_declarative import ResultSensor1, ResultSensor2, Base
+
+import requests
+
+engine = create_engine('sqlite:///restDatabase.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker()
+DBSession.bind = engine
+session = DBSession()
+
+app = Flask(__name__)
+api = Api(app)
+
+class Result1(Resource):
+    def get(self):
+        result1 = {'data': [dict(zip(tuple ("result 1") , session.query(ResultSensor1.resultSensor1).order_by('-id').first()))]}
+        return jsonify(result1)
+
+class Result2(Resource):
+    def get(self):
+        result2 = {'data': [dict(zip(tuple ("result 2") , session.query(ResultSensor2.resultSensor2).order_by('-id').first()))]}
+        return jsonify(result2)
+        
+api.add_resource(Result1, '/result1')
+api.add_resource(Result2, '/result2')
+
+if __name__ == '__main__':
+    app.run(port=5002)
